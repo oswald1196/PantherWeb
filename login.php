cefd9105ecf5fcf2a9fc0b0438cd46930f623cc1
Jesus Francisco Chinchillas Escarrega <jesus@panther-one.com>
2019-05-21 09:09:52 -0500

    Cambios visuales

--- login.php
+++ login.php
@@ -1,31 +1,28 @@
 <?php
 
-session_start();
+require 'conexion.php';
+
 $sUsuario= $_POST['usuario'];
 $sPassword= $_POST['pass'];
-
-require 'conexion.php';
 
 $sql = "SELECT * FROM CatMedico WHERE vchCorreoMedico = '$sUsuario' AND vchPassword = '$sPassword'";
 
 $result = mysqli_query($conn,$sql);
-
-if (mysqli_num_rows($result) > 0) {
 	
-    	while($row = mysqli_fetch_assoc($result)) {
-    	$vchUsuario = $row['vchCorreoMedico'];
-    	$vchPassword = $row['vchPassword'];
-    	if ($vchUsuario && $vchPassword){
-              $_SESSION["autenticado"]= "SI";
-    		header('Location: home.php');
+    	if($row = mysqli_fetch_array($result)) {
+    	if ($row['vchPassword'] == $sPassword){
+            session_start();
+            $_SESSION["autenticado"]= "SI";
+
+    		header('Location: home.php');            
     	}
         else{
-            header('Location: index.php');
-        }
-    
-    }
-    
-    }
+            header('Location: index.html');
+        } 
+    } else {
+            header('Location: index.html');
+
+    } 
 
 ?>
 
